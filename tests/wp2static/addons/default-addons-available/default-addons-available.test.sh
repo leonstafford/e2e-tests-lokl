eddfe11b37bee166bec9459a9ad6539c2ac8b96b
Leon Stafford <me@ljs.dev>
2020-10-01 04:41:34 +0930

    force failure

--- tests/wp2static/addons/default-addons-available/default-addons-available.test.sh
+++ tests/wp2static/addons/default-addons-available/default-addons-available.test.sh
@@ -7,6 +7,8 @@ echo "listing WP2Static addons"
 rm -f > /tmp/wp2static-addons-list.clioutput
 wp wp2static addons list > /tmp/wp2static-addons-list.clioutput
 
+cd "forcefail"
+
 echo "comparing expected and actual results"
 echo "using file to file cmp"
 if cmp -s /tmp/wp2static-addons-list.clioutput ./tests/wp2static/addons/default-addons-available/output/default-addons-list.clioutput
